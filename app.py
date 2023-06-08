a459149d330d372e57f10d19923d24384f4f132e
Mohamed <103029205+Mohr9@users.noreply.github.com>
2023-06-08 18:12:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ import numpy as np
 
 from bs4 import BeautifulSoup
 
-import nltk 
-from nltk.stem import WordNetLemmatizer, PorterStemmer
-from nltk.tokenize import word_tokenize, wordpunct_tokenize, RegexpTokenizer
-from nltk.corpus import words, stopwords
+# import nltk 
+# from nltk.stem import WordNetLemmatizer, PorterStemmer
+# from nltk.tokenize import word_tokenize, wordpunct_tokenize, RegexpTokenizer
+# from nltk.corpus import words, stopwords
 
 #import des packages pour la prédiction :
 
@@ -35,10 +35,10 @@ import streamlit as st
 # import os
 # os.environ["NLTK_DATA"]="/home/appuser/nltk_data"
 
-nltk.download("stopwords")
-from nltk_data.corpora import wordnet
-nltk.download("wordnet")
-nltk.download("corpora")
+# nltk.download("stopwords")
+# from nltk_data.corpora import wordnet
+# nltk.download("wordnet")
+# nltk.download("corpora")
 # nltk.download('all')
 # nltk.download("wordnet", "https://github.com/Mohr9/nlp_app_2/edit/master/app.py")
 
@@ -55,90 +55,6 @@ vect = pickle.load(open("vect","rb"))
 
 
 
-###### Fonctions prétraitement de texte  : 
-
-########### Fonction 1 : ###########
-####################################
-
-#fonction suppression des balises html : 
-
-def clean_balise(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    clean_text = soup.get_text()
-    return clean_text
-    
-########### Fonction 2 : ###########
-####################################
-
-
-def preprocessing(txt, list_rare_words = None,
-                  format_txt=False):
-
-    """
-    txt : contient le document au format str qui subira le preprocessing
-    format_txt : Si True cela renvoie une chaine de caractère, sinon une liste
-    list_rare_words : liste de token a fournir si on souhaite les supprimer
-    """
-    #tokenization et separation de la ponctuation
-    tokens = nltk.wordpunct_tokenize(txt)
-    
-    #suppression ponctuation
-    tokenizer = RegexpTokenizer(r"\w+")
-    tokens = tokenizer.tokenize(txt)
-    
-    #suppression majuscule : 
-    tokens = [w.lower() for w in tokens]
-    
-        
-    #suppression des chiffres : 
-    tokens = [w for w in tokens if not w.isnumeric()]
-
-    
-    #suppression stopwords : 
-    stopw = nltk.corpus.stopwords.words("english")
-    tokens = [w for w in tokens if w not in stopw]
-
-    #Supprime les tokens fournie dans la liste en hyperparametres
-    if list_rare_words:
-        tokens = [w for w in tokens if w not in list_rare_words]
-
-        
-        
-    #Lemmatization des mots s'ils n'appartiennent pas a la liste toptag : 
-    lemm = WordNetLemmatizer()
-    tmp_list = []
-
-    for i in tokens:
-        if i not in toptag: #si le token n'est pas dans la toptag liste alors on le lemmatize
-            tmp_list.append(lemm.lemmatize(i))
-        else: #sinon on conserve le token tel quel
-            tmp_list.append(i)
-    
-
-    #Suppression des mots token qui ne sont pas des mots dans le dictionnaire anglais 
-    #OU qui ne sont pas dans la liste des top tags à conserver :
-    
-    tokens = [w for w in tmp_list if w in eng_words or w in toptag]    
-    
-    
-    if format_txt:
-        tokens = " ".join(tokens)
-    return tokens
-
-
-########### Fonction 3 : ###########
-####################################
-
-
-#fonction d'application de notre prétraitement de texte :
-def cleaning(doc):
-    new_doc = preprocessing(doc, 
-                            list_rare_words = None, 
-                            format_txt=True, 
-                             )
-    return new_doc
-
-
     #Fonction pour transformer le texte de l'utilisateur en feature compatible avec notre modèle de ML:
 ###########################################################
 
@@ -151,8 +67,8 @@ def BoW(text):#le text doit etre une chaine de caractère en entrée
 ### Fonction qui, à partir du texte rentré par l'utilisateur, va retourner une prédiction de tag :
 
 def applying(text):
-    text = clean_balise(text) #utilisation des fonctions 2 de prétraitement de texte
-    text = cleaning(text)#ici le text devient une chaine de caractère
+#     text = clean_balise(text) #utilisation des fonctions 2 de prétraitement de texte
+#     text = cleaning(text)#ici le text devient une chaine de caractère
     text = BoW(text) # transformation du texte en feature compatible avec notre modèle de prédiction
     prediction = sgd.predict(text) #prediction du texte
     tag_pred = mlb.inverse_transform(prediction) #transformation de la target binarizée en target lisible 
